d684fb75b24183cc0dd276425957b35c1a27fa25
Florian Brandstetter on a development box <info@florianb.space>
2017-06-01 17:05:22 +0000

    update

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.sitename = 'Tunnelbroker';
 // MySQL
 config.mysql_host = 'localhost';
 config.mysql_user = 'root';
-config.mysql_pass = '';
+config.mysql_pass = '123123123';
 config.mysql_db = 'tunnelbroker';
 
 module.exports = config;
