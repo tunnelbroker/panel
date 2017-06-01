5555016fdb052e1483b506cabaaf0f0889b5dde3
Florian Brandstetter on a development box <info@florianb.space>
2017-06-01 16:52:27 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var cookieSession = require('cookie-session');
 var app = express();
+
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: 'session',
+  keys: [ 'asdf' ],
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
 var config = {
   'sitename' : 'Tunnelbroker'
@@ -9,6 +16,12 @@ var config = {
 
 app.get('/login/', function(req, res) {
   res.render('login', config);
+});
+
+app.get('/', function(req, res) {
+  if(!req.session.user_email) {
+    res.redirect('/login/');
+  }
 });
 
 app.listen(3000, function() {
