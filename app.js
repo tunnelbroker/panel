38170918be02264a6f02dee5bc41ef592416f667
Florian Brandstetter on a development box <info@florianb.space>
2017-06-01 16:43:27 +0000

    update

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+var config = {
+  'sitename' : 'Tunnelbroker'
+};
+
 app.get('/login/', function(req, res) {
-  res.render('login');
+  res.render('login', config);
 });
 
 app.listen(3000, function() {
