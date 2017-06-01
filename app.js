65a2fe456e2ea22c39a36b3fbc8a0a9bfcc5f00e
Florian Brandstetter on a development box <info@florianb.space>
2017-06-01 16:21:04 +0000

    update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
 
+app.get('/login/', function() {
+  res.render('login');
+});
 
 app.listen(3000, function() {
   console.log('Tunnelbroker panel is now listening on Port 3000.');
