eaa1a22b34c3e3b3a83a0c5aca4afe7e072c577d
Florian Brandstetter on a development box <info@florianb.space>
2017-06-01 16:22:41 +0000

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/login/', function(req, res) {
   res.render('login');
